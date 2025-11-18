663b2ad56765a0c142a9bbb68dae8e344eb2e6c8
Jessevdonck <jesse.vaerendonck@student.hogent.be>
2025-11-18 20:29:30 +0100

    kalken project added

--- app/data/projects.js
+++ app/data/projects.js
@@ -1,46 +1,72 @@
 const projects = [
-    {
-      "title": "Webservice for KSKcolle",
-      "description": "A modern website for KSKcolle, a chess club, featuring tournament management, player administration, and an up-to-date calendar.",
-      "longDescription": "This website is a complete overhaul for KSKcolle, a chess club that previously relied on an outdated website. The new website includes a backend built with Koa, making it easy to manage tournaments, players, and the calendar. The frontend, developed in NextJS, offers a warm and modern design that is user-friendly and easy to navigate. The goal was to create an intuitive experience for both members and visitors.",
-      "image": "/projects/kskcolle.png",
-      "technologies": ["NextJS", "Koa", "Node.js", "MySQL", "React", "TailwindCSS", "TypeScript"],
-      "githubUrl": "https://github.com/Jessevdonck/KSKColle",
-      "liveUrl": "https://kskcolle.be"
-    },
-    {
-      "title": "Kingdomino",
-      "description": "A digitalized version of the popular board game Kingdomino, developed as a group project during my first year of college.",
-      "longDescription": "This project marked my first experience developing a complete project in Java. The goal was to create a digital version of the board game Kingdomino, using JavaFX for the graphical interface and MySQL for storing game data. The project required strong teamwork and a structured approach to translate the complexity of the game into a functional application. This project significantly improved my foundational knowledge of Java, object-oriented programming, and database management.",
-      "image": "/projects/kingdomino.jpg",
-      "technologies": ["Java", "JavaFX", "MySQL"],
-      "githubUrl": "https://github.com/Jessevdonck/Kingdomino",
-      },
-      // {
-      //   "title": "Personal Portfolio",
-      //   "description": "A modern personal portfolio website to showcase my projects and skills, built with Next.js and featuring a contact form powered by Formspree.",
-      //   "longDescription": "This portfolio website was developed to present my work and skills in a clean and professional manner. Built with Next.js, it offers a fast and responsive user experience. The contact form is integrated with Formspree, allowing visitors to send messages directly to my email. The project highlights my ability to work with modern web technologies, including React, TailwindCSS, and TypeScript, while also demonstrating my attention to design and user experience.",
-      //   "image": "/projects/portfolio.png",
-      //   "technologies": ["Next.js", "React", "TailwindCSS", "TypeScript", "Formspree"],
-      //   "liveUrl": "https://jessevdonck.com"
-      // },
-        {
-          "title": "Shopfloor Application",
-          "description": "A shopfloor application for Delaware, developed as part of a school project. The app manages machines, users, maintenance, and notifications.",
-          "longDescription": "As part of a school project, I built a shopfloor application for Delaware, acting as the real client. The app, developed with Java, JavaFX, JPA, and MariaDB, allows management of machines, users, maintenance, and notifications, with real-time notification support.",
-          "image": "/projects/shopfloorDelaware.png",
-          "technologies": ["Java", "JPA", "JavaFX", "MariaDB"],
-          "githubUrl": "https://github.com/Jessevdonck/shopfloor-delaware",
-        },
-        {
-          "title": "Reagent Gamejam 2025 - The Not-So-Handyman",
-          "description": "A chaotic 2D game made during the first edition of the Reagent Gamejam, where we took first place with our darkly humorous take on the theme 'tinkering'.",
-          "longDescription": "The Not-So-Handyman was created during the inaugural Reagent Gamejam, a fast-paced game development competition for teams of 2–5 people, built around the theme 'tinkering'. Developed together with Jordan Bentkowski, the game follows a man struggling with a blackjack addiction who does odd jobs at his grandma’s house to fund his next trip to the casino. Players must complete tasks as quickly as possible to earn money. Once satisfied with their earnings, they can head to the casino to play blackjack, but if they lose it all, it’s back to fixing bad wiring and unclogging toilets. I was involved in every part of the creative process, from coding in Unity (C#), crafting pixel art in Aseprite, composing music and sound design in FL Studio, to polishing the overall feel and pacing of the gameplay.",
-          "image": "/projects/reagentGameJam2025.png",
-          "technologies": ["Unity", "C#", "Aseprite", "FL Studio"],
-          "liveUrl": "https://jessevdonck.itch.io/the-not-so-handyman",
-          "githubUrl": "https://github.com/Jessevdonck/Reagent-GameJam2025"
-        }
-  ]
+  {
+    title: "Webservice for KSKcolle",
+    description:
+      "A modern website for KSKcolle, a chess club, featuring tournament management, player administration, and an up-to-date calendar.",
+    longDescription:
+      "This website is a complete overhaul for KSKcolle, a chess club that previously relied on an outdated website. The new website includes a backend built with Koa, making it easy to manage tournaments, players, and the calendar. The frontend, developed in NextJS, offers a warm and modern design that is user-friendly and easy to navigate. The goal was to create an intuitive experience for both members and visitors.",
+    image: "/projects/kskcolle.png",
+    technologies: [
+      "NextJS",
+      "Koa",
+      "Node.js",
+      "MySQL",
+      "React",
+      "TailwindCSS",
+      "TypeScript",
+    ],
+    githubUrl: "https://github.com/Jessevdonck/KSKColle",
+    liveUrl: "https://kskcolle.be",
+  },
+  {
+    title: "Kingdomino",
+    description:
+      "A digitalized version of the popular board game Kingdomino, developed as a group project during my first year of college.",
+    longDescription:
+      "This project marked my first experience developing a complete project in Java. The goal was to create a digital version of the board game Kingdomino, using JavaFX for the graphical interface and MySQL for storing game data. The project required strong teamwork and a structured approach to translate the complexity of the game into a functional application. This project significantly improved my foundational knowledge of Java, object-oriented programming, and database management.",
+    image: "/projects/kingdomino.jpg",
+    technologies: ["Java", "JavaFX", "MySQL"],
+    githubUrl: "https://github.com/Jessevdonck/Kingdomino",
+  },
+  // {
+  //   "title": "Personal Portfolio",
+  //   "description": "A modern personal portfolio website to showcase my projects and skills, built with Next.js and featuring a contact form powered by Formspree.",
+  //   "longDescription": "This portfolio website was developed to present my work and skills in a clean and professional manner. Built with Next.js, it offers a fast and responsive user experience. The contact form is integrated with Formspree, allowing visitors to send messages directly to my email. The project highlights my ability to work with modern web technologies, including React, TailwindCSS, and TypeScript, while also demonstrating my attention to design and user experience.",
+  //   "image": "/projects/portfolio.png",
+  //   "technologies": ["Next.js", "React", "TailwindCSS", "TypeScript", "Formspree"],
+  //   "liveUrl": "https://jessevdonck.com"
+  // },
+  {
+    title: "Shopfloor Application",
+    description:
+      "A shopfloor application for Delaware, developed as part of a school project. The app manages machines, users, maintenance, and notifications.",
+    longDescription:
+      "As part of a school project, I built a shopfloor application for Delaware, acting as the real client. The app, developed with Java, JavaFX, JPA, and MariaDB, allows management of machines, users, maintenance, and notifications, with real-time notification support.",
+    image: "/projects/shopfloorDelaware.png",
+    technologies: ["Java", "JPA", "JavaFX", "MariaDB"],
+    githubUrl: "https://github.com/Jessevdonck/shopfloor-delaware",
+  },
+  {
+    title: "Reagent Gamejam 2025 - The Not-So-Handyman",
+    description:
+      "A chaotic 2D game made during the first edition of the Reagent Gamejam, where we took first place with our darkly humorous take on the theme 'tinkering'.",
+    longDescription:
+      "The Not-So-Handyman was created during the inaugural Reagent Gamejam, a fast-paced game development competition for teams of 2–5 people, built around the theme 'tinkering'. Developed together with Jordan Bentkowski, the game follows a man struggling with a blackjack addiction who does odd jobs at his grandma’s house to fund his next trip to the casino. Players must complete tasks as quickly as possible to earn money. Once satisfied with their earnings, they can head to the casino to play blackjack, but if they lose it all, it’s back to fixing bad wiring and unclogging toilets. I was involved in every part of the creative process, from coding in Unity (C#), crafting pixel art in Aseprite, composing music and sound design in FL Studio, to polishing the overall feel and pacing of the gameplay.",
+    image: "/projects/reagentGameJam2025.png",
+    technologies: ["Unity", "C#", "Aseprite", "FL Studio"],
+    liveUrl: "https://jessevdonck.itch.io/the-not-so-handyman",
+    githubUrl: "https://github.com/Jessevdonck/Reagent-GameJam2025",
+  },
+  {
+    title: "Website for chess club Kalken",
+    description: "A static website for the chess club in Kalken.",
+    longDescription:
+      "I made this website for the chess club in Kalken. They are a starting chess club that desperately needed a website. This is why I provided a static website that suited all of their needs",
+    image: "/projects/kalken.png",
+    technologies: ["React", "NextJS", "JavaScript", "TailwindCSS"],
+    liveUrl: "www.schaakclubkalken.be",
+    githubUrl: "http://github.com/Jessevdonck/Schaakclub-Kalken/",
+  },
+];
 
-  export default projects;
+export default projects;
